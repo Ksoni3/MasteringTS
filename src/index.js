8dd0bea3c25dbde04827b48deb88e21a5e322329
Kamal Soni <94995608+Ksoni3@users.noreply.github.com>
2023-09-02 18:04:13 +1000

    compiled into js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+var a = "Kamal";
+console.log(a);
+//basic types
+var b = 25;
+var check = true;
+// avoid using any as there would be no point of using ts 
+var checkAny;
+// Union
+var age;
+// doesnt give error
+age = 10;
+age = 'ten';
