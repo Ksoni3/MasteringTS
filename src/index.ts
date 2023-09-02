a384ab79d9c231d51b6e279e649dc7452ac94124
Kamal Soni <94995608+Ksoni3@users.noreply.github.com>
2023-09-02 18:04:02 +1000

    basics

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+const a: string = "Kamal";
+console.log(a)
+
+//basic types
+
+let b = 25;
+let check:boolean = true;
+
+
+// avoid using any as there would be no point of using ts 
+let checkAny:any;
+
+// Union
+
+let age : string | number;
+
+
+// doesnt give error
+age = 10 ;  
+age = 'ten';
+
+
+
+
+
+
+
+
+
