e80882653bd40440477a31edb145c4824d3f1313
Kamal Soni <94995608+Ksoni3@users.noreply.github.com>
2023-09-03 22:59:32 +1000

    generics

---
+++ src/Generics.ts
@@ -0,0 +1,35 @@
+// Generics
+
+const fun = (n:number):number =>{
+    return n;
+}
+
+
+const fun1 = <T> (n:T): T =>{
+    return n;
+}
+
+type PersonType = {
+    name: string,
+    age: number
+}
+
+const person1: PersonType = {
+    name: 'Kamal',
+    age: 10
+}
+
+const ans = fun1<PersonType>(person1)
+ans.name;
+
+//two parameters
+
+const fun2 = <T, U> (n: T, o:U):{n:T, o:U}=>{
+    return {n,o}
+
+}
+
+const ans2 = fun2 <number, string> (20, 'Helloe')
+ans2.n
+
+
